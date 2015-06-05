31d7d4c78f3b7f4b95ce709e4dfccd1a2a556809
Gourav Kumar <gourav2711@gmail.com>
2015-06-05 16:46:41 +0530

    updated code for loading libraries

--- plot3.R
+++ plot3.R
@@ -2,7 +2,12 @@ power1 <- read.csv("household_power_consumption.txt", sep=";", nrows = 10)
 classes <- sapply(power1, class)
 power1 <- read.table("household_power_consumption.txt", sep=";", 
                      header = TRUE, na.strings = "?", colClasses = classes)
-library(lubridate) #in case lubridate is not installed, execute: install.packages("lubridate")
+
+#For the following two statements you will need to have lubridate and dplyr packages installed
+#In case you do not have these packages installed, run the following two statements (Internet connection rquired):
+#install.packages("lubridate")
+#install.packages("dplyr")					 
+					 
 power1$Date <- dmy(power1$Date)
 power2 <- filter(power1, year(Date) == 2007 & month(Date) == 2 & (day(Date)==1 | day(Date)==2))
 plot(power2$Sub_metering_1, type = "l", xlab="", ylab="Energy sub metering", xaxt = "n")
